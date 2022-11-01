b1c9898278d7ba8ca68583342bece05850ccafe7
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-11-01 15:34:32 +0200

    Replay1
    Replay1

---
+++ Replay1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Replay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Please enter yout name");
+            name = Console.ReadLine();
+            Console.Write(name);
+        }
+    }
+}
